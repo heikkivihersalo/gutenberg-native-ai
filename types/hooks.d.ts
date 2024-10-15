edeed647fb59d5499b31716ed6302fe17bfcf724
Heikki Vihersalo <heikki@vihersalo.fi>
2024-10-15 12:02:22 +0300

    add hooks module

---
+++ types/hooks.d.ts
@@ -0,0 +1,20 @@
+declare module '@hooks' {
+	export type ChatGPTPromptInput = {
+		prompt: FormDataEntryValue | null;
+		selection?: string;
+	};
+
+	export const useChatGPT: () => {
+		getText: ({ prompt, selection }: ChatGPTPromptInput) => Promise<string>;
+		getImage: ({ prompt }: ChatGPTPromptInput) => Promise<ChatGPTImage[]>;
+	};
+
+	export const useAdminForm: (props: AdminFormProps) => {
+		formData: AdminFormData;
+		setFormData: (data: AdminFormData) => void;
+		handleChange: (
+			event: React.MouseEvent<HTMLInputElement, MouseEvent>
+		) => void;
+		handleSave: ({ data }: { data: AdminFormData }) => void;
+	};
+}
