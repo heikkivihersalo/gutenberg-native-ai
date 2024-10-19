1ae39441887488bc20c2282e1c841db162df6ffd
Heikki Vihersalo <heikki@vihersalo.fi>
2024-10-19 12:20:25 +0300

    fix comment tab to spaces linter problem

--- gutenberg-native-ai.php
+++ gutenberg-native-ai.php
@@ -18,7 +18,7 @@
  * Text Domain:       gutenberg-native-ai
  * Domain Path:       /languages
  * Requires at least: 6.5.2
- * Requires PHP: 	  8.2
+ * Requires PHP:      8.2
  */
 
 namespace Kotisivu\Gutenberg_Native_AI;
