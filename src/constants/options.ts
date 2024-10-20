7bcb26f960783b0b0e0eeb5fa7b7c21e6f7b788c
Heikki Vihersalo <heikki@vihersalo.fi>
2024-10-20 21:01:14 +0300

    initial commit

---
+++ src/constants/options.ts
@@ -0,0 +1,50 @@
+import { __ } from '@wordpress/i18n';
+
+export const TONE_OF_VOICE = [
+	{ value: 'none', label: __('None', 'gutenberg-native-ai') },
+	{
+		value: 'friendly',
+		label: __('Friendly and Professional', 'gutenberg-native-ai'),
+	},
+	{
+		value: 'authoritative',
+		label: __('Authoritative and Informative', 'gutenberg-native-ai'),
+	},
+	{
+		value: 'urgent',
+		label: __('Urgent and Persuasive', 'gutenberg-native-ai'),
+	},
+	{
+		value: 'casual',
+		label: __('Casual and Conversational', 'gutenberg-native-ai'),
+	},
+	{
+		value: 'trustworthy',
+		label: __('Professional and Trustworthy', 'gutenberg-native-ai'),
+	},
+	{
+		value: 'humorous',
+		label: __('Humorous and Informal', 'gutenberg-native-ai'),
+	},
+	{
+		value: 'straightforward',
+		label: __('Professional and Straightforward', 'gutenberg-native-ai'),
+	},
+	{
+		value: 'empathetic',
+		label: __('Serious and Empathetic', 'gutenberg-native-ai'),
+	},
+	{
+		value: 'enthusiastic',
+		label: __('Positive and Enthusiastic', 'gutenberg-native-ai'),
+	},
+	{
+		value: 'expert',
+		label: __('Authoritative and Professional', 'gutenberg-native-ai'),
+	},
+	{ value: 'funny', label: __('Casual and Funny', 'gutenberg-native-ai') },
+	{
+		value: 'professional',
+		label: __('Authoritative and Expert', 'gutenberg-native-ai'),
+	},
+];
