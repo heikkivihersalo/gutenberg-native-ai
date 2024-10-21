de33c4928eddc7c06fba6d115da560b58729508f
Heikki Vihersalo <heikki@vihersalo.fi>
2024-10-21 10:37:13 +0300

    moved component from pages

--- src/features/admin/index.tsx
+++ src/features/admin/index.tsx
@@ -2,20 +2,40 @@
  * WordPress dependencies
  */
 import { createRoot } from '@wordpress/element';
-import { __ } from '@wordpress/i18n';
+import { __, sprintf } from '@wordpress/i18n';
 import domReady from '@wordpress/dom-ready';
 
 /**
  * Internal dependencies
  */
-import ChatGPT from './pages/ChatGPT';
+import { useAdminForm } from '@hooks';
+import { OptionBody, OptionGroup } from './components/containers';
+import { Form, FormHead, FormBody, FormButton } from './components/form';
+import { Input, Select } from './components/inputs';
+
+import { TONE_OF_VOICE } from '@constants/options';
+
+import type { ChatGPTAdminRequestData } from 'types/admin';
+
 import './style.css';
 
 /**
  * Main component
  * @return {JSX.Element} Main component
  */
-const Main = (): JSX.Element => {
+const Main = (): JSX.Element | null => {
+	const { formData, handleChange, handleSave } = useAdminForm({
+		path: 'gutenberg-native-ai/v1/settings',
+		nonce: window.GUTENBERG_NATIVE_AI?.nonce,
+	});
+
+	/**
+	 * Return early if form is not set and loaded
+	 */
+	if (!formData) {
+		return null;
+	}
+
 	return (
 		<>
 			<header>
@@ -23,7 +43,64 @@ const Main = (): JSX.Element => {
 					{__('Gutenberg Native AI Settings', 'gutenberg-native-ai')}
 				</h1>
 			</header>
-			<ChatGPT />
+			<OptionBody
+				name={__('ChatGPT', 'gutenberg-native-ai')}
+				description={sprintf(
+					/* translators: %s: ChatGPT dashboard URL */
+					__(
+						'Configure the ChatGPT API settings. You can get the API key from the ChatGPT dashboard. Access the dashboard <a href="%s">here</a>.',
+						'gutenberg-native-ai'
+					),
+					'https://platform.openai.com/api-keys'
+				)}
+			>
+				<OptionGroup>
+					<Form onSave={() => handleSave({ data: formData })}>
+						<FormHead
+							name={__('API Settings', 'gutenberg-native-ai')}
+						/>
+						<FormBody>
+							<Input
+								type="text"
+								label={__('Model', 'gutenberg-native-ai')}
+								name="model"
+								placeholder="gpt-4o-mini"
+								value="gpt-4o-mini"
+								disabled={true}
+							/>
+							<Input
+								type="text"
+								label={__('API Key', 'gutenberg-native-ai')}
+								name="api_key"
+								placeholder="sk-proj-xxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+								value={
+									(formData as ChatGPTAdminRequestData)
+										.api_key
+								}
+								onChange={
+									handleChange as React.ChangeEventHandler<HTMLInputElement>
+								}
+							/>
+							<Select
+								label={__(
+									'Tone of Voice',
+									'gutenberg-native-ai'
+								)}
+								name="tone_of_voice"
+								value={
+									(formData as ChatGPTAdminRequestData)
+										.tone_of_voice
+								}
+								options={TONE_OF_VOICE}
+								onChange={
+									handleChange as React.ChangeEventHandler<HTMLSelectElement>
+								}
+							/>
+						</FormBody>
+						<FormButton />
+					</Form>
+				</OptionGroup>
+			</OptionBody>
 		</>
 	);
 };
