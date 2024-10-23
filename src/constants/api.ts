fafa25eee036f1326f285a03c7597a416a4b5d35
Heikki Vihersalo <heikki@vihersalo.fi>
2024-10-23 17:10:56 +0300

    add translate text command

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -2,6 +2,7 @@ export const API_PATH = {
 	GET_SETTINGS: 'gutenberg-native-ai/v1/settings',
 	SET_SETTINGS: 'gutenberg-native-ai/v1/settings',
 	GENERATE_TEXT: 'gutenberg-native-ai/v1/text/generate',
+	TRANSLATE_TEXT: 'gutenberg-native-ai/v1/text/translate',
 	GENERATE_IMAGE: 'gutenberg-native-ai/v1/image/generate',
 	SAVE_IMAGE: 'gutenberg-native-ai/v1/image/save',
 };
