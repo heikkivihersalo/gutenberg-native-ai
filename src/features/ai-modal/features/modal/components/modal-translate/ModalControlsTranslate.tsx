c5cca37b5792f198e6bbc7ae28779a4721991d18
Heikki Vihersalo <heikki@vihersalo.fi>
2024-10-23 18:32:44 +0300

    fix to use correct data store

--- src/features/ai-modal/features/modal/components/modal-translate/ModalControlsTranslate.tsx
+++ src/features/ai-modal/features/modal/components/modal-translate/ModalControlsTranslate.tsx
@@ -10,18 +10,13 @@ import { useSelect, useDispatch } from '@wordpress/data';
 import { useChatGPT } from '@hooks';
 
 import Form from '../form/Form';
-import FormInput from '../form/FormInput';
 import FormSubmitButton from '../form/FormSubmitButton';
 import LanguageSelector from './LanguageSelector';
 import CloseButton from '../CloseButton';
 import WarningText from '../WarningText';
 import Settings from '../../../settings';
 
-import {
-	addBlocksToEditor,
-	parseMarkdownToBlocks,
-	replateSelectedText,
-} from '../../../../utils';
+import { closeModal, replateSelectedText } from '../../../../utils';
 
 import { MODAL_STATUS } from '@constants/modal';
 import { DATA_STORE } from '@constants/stores';
@@ -40,27 +35,20 @@ import styles from '../../index.module.css';
  * @return {JSX.Element} Popover component
  */
 const ModalControlsTranslate = (): JSX.Element | null => {
-	const { status, selection, settings } = useSelect((select: WPAny) => {
-		return {
-			status: select(DATA_STORE).getStatus() as ModalStatus,
-			selection: select(DATA_STORE).getSelection() as ModalSelection,
-			settings: select(DATA_STORE).getSettings() as ModalSettings,
-			languages: select(DATA_STORE).getLanguages() as ModalLanguages,
-		};
-	}, []);
+	const { status, selection, settings, languages } = useSelect(
+		(select: WPAny) => {
+			return {
+				status: select(DATA_STORE).getStatus() as ModalStatus,
+				selection: select(DATA_STORE).getSelection() as ModalSelection,
+				settings: select(DATA_STORE).getSettings() as ModalSettings,
+				languages: select(DATA_STORE).getLanguages() as ModalLanguages,
+			};
+		},
+		[]
+	);
 
-	const { setStatus, setSelection } = useDispatch(DATA_STORE);
+	const { setStatus } = useDispatch(DATA_STORE);
 	const { translate } = useChatGPT();
-
-	const handleCLose = () => {
-		setStatus(MODAL_STATUS.INITIAL);
-		setSelection({
-			block: null,
-			text: '',
-			start: 0,
-			end: 0,
-		});
-	};
 
 	/**
 	 * Handle generate content
@@ -76,12 +64,12 @@ const ModalControlsTranslate = (): JSX.Element | null => {
 		 * Handle prompt validation
 		 * TODO: window.alert is not recommended, use a proper validation method
 		 */
-		if (selection.start !== selection.end) {
+		if (selection.start === selection.end) {
 			// eslint-disable-next-line no-alert
 			window.alert(
 				__('Please select text to translate', 'gutenberg-native-ai')
 			);
-			setStatus(MODAL_STATUS.INITIAL);
+			closeModal();
 			return;
 		}
 
@@ -90,8 +78,8 @@ const ModalControlsTranslate = (): JSX.Element | null => {
 		 */
 		const aiContent = await translate({
 			selection: selection.text,
-			languageFrom: settings.languages.from,
-			languageTo: settings.languages.to,
+			languageFrom: languages.from,
+			languageTo: languages.to,
 		});
 
 		replateSelectedText({
@@ -101,7 +89,7 @@ const ModalControlsTranslate = (): JSX.Element | null => {
 			end: selection.end,
 		});
 
-		handleCLose();
+		closeModal();
 	};
 
 	return (
@@ -112,7 +100,7 @@ const ModalControlsTranslate = (): JSX.Element | null => {
 			<div className={styles.controlContainer}>
 				<LanguageSelector />
 				<FormSubmitButton status={status} />
-				<CloseButton closeCallback={handleCLose} />
+				<CloseButton closeCallback={closeModal} />
 				<Settings />
 			</div>
 			<WarningText />
