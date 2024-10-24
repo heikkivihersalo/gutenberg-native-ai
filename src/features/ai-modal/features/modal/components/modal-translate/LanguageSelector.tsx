67fd2735802151cc30c8298f1aeacb65bab2494f
Heikki Vihersalo <heikki@vihersalo.fi>
2024-10-24 09:38:54 +0300

    disable translate when no text is selected

--- src/features/ai-modal/features/modal/components/modal-translate/LanguageSelector.tsx
+++ src/features/ai-modal/features/modal/components/modal-translate/LanguageSelector.tsx
@@ -10,7 +10,7 @@ import { useSelect, useDispatch } from '@wordpress/data';
 import { AVAILABLE_LANGUAGES } from '@constants/options';
 import { DATA_STORE } from '@constants/stores';
 
-import type { ModalLanguages } from 'types/modal';
+import type { ModalLanguages, ModalSelection } from 'types/modal';
 
 import styles from '../../index.module.css';
 
@@ -19,68 +19,83 @@ import styles from '../../index.module.css';
  * @return {JSX.Element} LanguageSelector component
  */
 const LanguageSelector = (): JSX.Element => {
-	const { languages } = useSelect((select: WPAny) => {
+	const { languages, selection } = useSelect((select: WPAny) => {
 		return {
 			languages: select(DATA_STORE).getLanguages() as ModalLanguages,
+			selection: select(DATA_STORE).getSelection() as ModalSelection,
 		};
 	}, []);
 
 	const { setLanguages } = useDispatch(DATA_STORE);
 
+	const hasSelection = selection.start !== selection.end;
+
 	return (
 		<div className={styles.languageSelector}>
-			<div className={styles.languageSelectorInputWrapper}>
-				<label
-					className={styles.languageSelectorLabel}
-					htmlFor="language-from"
-				>
-					{__('From', 'gutenberg-native-ai')}
-				</label>
-				<select
-					id="language-from"
-					className={styles.languageSelectorInput}
-					name="language-from"
-					value={languages.from || undefined}
-					onChange={(e) =>
-						setLanguages({
-							...languages,
-							from: e.target.value,
-						})
-					}
-				>
-					{AVAILABLE_LANGUAGES.map((option) => (
-						<option key={option.value} value={option.value}>
-							{option.label}
-						</option>
-					))}
-				</select>
-			</div>
-			<div className={styles.languageSelectorInputWrapper}>
-				<label
-					className={styles.languageSelectorLabel}
-					htmlFor="language-to"
-				>
-					{__('To', 'gutenberg-native-ai')}
-				</label>
-				<select
-					id="language-to"
-					className={styles.languageSelectorInput}
-					name="language-to"
-					value={languages.to || undefined}
-					onChange={(e) =>
-						setLanguages({
-							...languages,
-							to: e.target.value,
-						})
-					}
-				>
-					{AVAILABLE_LANGUAGES.map((option) => (
-						<option key={option.value} value={option.value}>
-							{option.label}
-						</option>
-					))}
-				</select>
-			</div>
+			{hasSelection ? (
+				<>
+					<div className={styles.languageSelectorInputWrapper}>
+						<label
+							className={styles.languageSelectorLabel}
+							htmlFor="language-from"
+						>
+							{__('From', 'gutenberg-native-ai')}
+						</label>
+						<select
+							id="language-from"
+							className={styles.languageSelectorInput}
+							name="language-from"
+							value={languages.from || undefined}
+							onChange={(e) =>
+								setLanguages({
+									...languages,
+									from: e.target.value,
+								})
+							}
+							disabled={!hasSelection}
+						>
+							{AVAILABLE_LANGUAGES.map((option) => (
+								<option key={option.value} value={option.value}>
+									{option.label}
+								</option>
+							))}
+						</select>
+					</div>
+					<div className={styles.languageSelectorInputWrapper}>
+						<label
+							className={styles.languageSelectorLabel}
+							htmlFor="language-to"
+						>
+							{__('To', 'gutenberg-native-ai')}
+						</label>
+						<select
+							id="language-to"
+							className={styles.languageSelectorInput}
+							name="language-to"
+							value={languages.to || undefined}
+							onChange={(e) =>
+								setLanguages({
+									...languages,
+									to: e.target.value,
+								})
+							}
+						>
+							{AVAILABLE_LANGUAGES.map((option) => (
+								<option key={option.value} value={option.value}>
+									{option.label}
+								</option>
+							))}
+						</select>
+					</div>
+				</>
+			) : (
+				<p className={styles.languageSelectorWarningText}>
+					{__(
+						'You currently have no text selected. Please close the modal and select text to create translations.',
+						'gutenberg-native-ai'
+					)}
+				</p>
+			)}
 		</div>
 	);
 };
